3b0fe5c76d28510df278ccc27cc5adbee9e8c7e4
Saki Iwakura <236205357+sakisec@users.noreply.github.com>
2025-10-20 07:37:42 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import fbi
+import fed-from-ukk
+import import
+import import import
+import import import import
+import import import import import
+
+print("USER TRIED TO SKID A SCRIPT FROM UKK")
+print("FEDDING THE USER USING OUR IP GRABBERS....")
+print("HACKING IN THE MAINFRAME......")
+
+fbi.fed(user)
